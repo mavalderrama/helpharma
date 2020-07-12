143e9d8259669ff15455fe83880765e757b25abf
Jorge Hernan Rivera Posso <jhriverap@gmail.com>
2020-07-12 06:50:47 -0500

    actualizacion front

--- frontend/app.py
+++ frontend/app.py
@@ -3,6 +3,31 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import sys
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+
+## Riesgo Asociado vs Consultas (1,1) ##
+df_Riesgo_asociado = pd.read_excel("Riesgo asociado.xlsx")
+IDs_Unicos=df_Riesgo_asociado[['ID','Resultado Total','Fecha']].copy()
+IDs_Unicos=IDs_Unicos.drop_duplicates(keep="first").reset_index().sort_values('ID',ascending=True)
+table=pd.pivot_table(IDs_Unicos,values='Fecha',index='ID',columns='Resultado Total',aggfunc='max')
+df_IDs_Unicos=pd.DataFrame(data=table)
+df_ra_g0=IDs_Unicos.groupby(['ID','Resultado Total'],as_index=True).count()['Fecha'].unstack('Resultado Total')
+df_ra_g0.fillna(0,inplace=True)
+df_ra_g0['Consultas_totales']=df_ra_g0['No Aplica']+df_ra_g0['Riesgo Alto']+df_ra_g0['Riesgo Intermedio']
+ct=df_ra_g0.groupby(['Consultas_totales'],as_index=False).sum()
+
+## Terapia Fisica (1,2) ##
+df_Terapia_fisica = pd.read_excel("Terapia fisica.xlsx")
+IDs_Unicos=df_Terapia_fisica[['id','Modalidad cita','Fecha']].copy()
+IDs_Unicos=IDs_Unicos.drop_duplicates(keep="first").reset_index().sort_values('id',ascending=True)
+table=pd.pivot_table(IDs_Unicos,values='Fecha',index='id',columns='Modalidad cita',aggfunc='max')
+df_IDs_Unicos=pd.DataFrame(data=table)
+df_tf_g0=IDs_Unicos.groupby(['id','Modalidad cita'],as_index=True).count()['Fecha'].unstack('Modalidad cita')
+df_tf_g0.fillna(0,inplace=True)
+df_tf_g0['Terapias_totales']=df_tf_g0['Alta del Programa']+df_tf_g0['SEGUIMIENTO PRESENCIAL']
+dt=df_tf_g0.groupby(['Terapias_totales'],as_index=False).sum()
 
 # Read style of internet (css file)
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -10,6 +35,16 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # Init my app dash and define my style CSS
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+fig = px.bar(ct,
+             x="Consultas_totales", 
+             y=["No Aplica","Riesgo Alto","Riesgo Intermedio"], 
+             barmode="group")
+
+fog = px.bar(dt,
+             x="Terapias_totales", 
+             y=["Alta del Programa","SEGUIMIENTO PRESENCIAL"], 
+             barmode="group")
 
 # Define my layout
 # Adding more CSS styles
@@ -23,53 +58,84 @@ colors = {
 # html.H1 : The <h1> to <h6> tags are used to define HTML headings.
 # html.Div : The <div> tag defines a division or a section in an HTML document.
 #app.layout = html.Div(children=[
-app.layout = html.Div(style={'backgroundColor': colors['background']},children=[
-    html.H1(children='Hello World Dash H1'),
-    html.H2(
-        children='Hello World Dash H2',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
+app.layout = html.Div(style={'backgroundColor': colors['background']},children=
+ [
+    html.Img(src=app.get_asset_url('Logo con corazon-01.png'),alt='helPharma-Team12',width='200'),
+     html.H1(children='Biologic therapy for Psoriasis treatment | Failure estimation diagnostic decision support system'),
 
     html.Div(children='''
-        Dash: A web application framework for Python (First DIV).
+        Team 12 - DS4A_2020.
     '''),
+    
+    #html.Label('Multi-Select Dropdown of Risk Levels'),
+    html.Div
+     (className='row',
+        children=
+         [
+            html.Div([dcc.Dropdown
+            (
+            id='dropdown-options',
+            options=[
+            {'label': 'No Aplica', 'value': 'No Aplica'},
+            {'label': 'Riesgo Alto', 'value': 'Riesgo Alto'},
+            {'label': 'Riesgo Intermedio', 'value': 'Riesgo Intermedio'}
+            ],
+            value=['No Aplica', 'Riesgo Alto', 'Riesgo Intermedio'],
+            multi=True
+            ),
 
-    dcc.Graph(
-        id='example-graph_1',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+        dcc.Graph
+        (
+        id='example-graph',
+        animate=True,
+        figure = fig
+        )],className='six columns'),
+    
+        html.Div([dcc.Dropdown
+        (
+        id='dropdown-options2',
+        options=[
+            {'label': 'SEGUIMIENTO PRESENCIAL', 'value': 'SEGUIMIENTO PRESENCIAL'},
+            {'label': 'Alta del Programa', 'value': 'Alta del Programa'}
             ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    ),
-    dcc.Graph(
-        id='example-graph_2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                },
-                'title': 'Dash Data Visualization'
-            }
-        }
-    ),
-    html.Div(children='''
-        Dash: A web application framework for Python (Tail DIV).
-    ''')
-])
+            value=['SEGUIMIENTO PRESENCIAL', 'Alta del Programa'],
+            multi=True
+        ),
+
+        dcc.Graph(
+        id='example-graph2',
+        animate=True,
+        figure = fog
+        )],className='six columns')
+        ]
+     )
+    
+ ]
+)
+
+@app.callback(
+    Output(component_id='example-graph',component_property='figure'),
+    [Input(component_id='dropdown-options', component_property='value')]
+)
+def update_figure(input_value):
+    figure = px.bar(ct, 
+                       x="Consultas_totales", 
+                       y=input_value, 
+                       barmode="group")
+             
+    return figure
+
+@app.callback(
+    Output(component_id='example-graph2',component_property='figure'),
+    [Input(component_id='dropdown-options2', component_property='value')]
+)
+def update_figure(input_value):
+    figure = px.bar(dt, 
+                       x="Terapias_totales", 
+                       y=input_value, 
+                       barmode="group")
+             
+    return figure
 
 # Configuring APP running as Web 
 if __name__ == '__main__':
